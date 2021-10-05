df93b9b3ffc0c20103900d54b9ed29c6d26c8ba1
Josh-Cena <sidachen2003@gmail.com>
2021-10-05 16:15:45 +0800

    Improve haskell example
    Signed-off-by: Josh-Cena <sidachen2003@gmail.com>

--- demo/fibonacci/fibonacci.hs
+++ demo/fibonacci/fibonacci.hs
@@ -1,7 +1,7 @@
 fib i
   | i == 0 || i == 1 = 1
-  | otherwise        = fib(i - 1) + fib(i - 2)
-sigma l r expr = sum(map expr [l..r])
+  | otherwise        = fib (i - 1) + fib (i - 2)
+sigma l r expr = sum (map expr [l..r])
 
 main = do
   putStrLn (show (sum (map fib [2..9])))
